357353621e39ee87325f53974d4b8c35bd2f1ed1
Petr Savichev (proton) <psavichev@gmail.com>
2021-10-03 17:58:55 +0300

    rerender on inputs change

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <Field :width="width" :height="height" :minesCount="minesCount" />
-  <br/>
   <div>
-    Size: 
-    <input type="number" v-model="width"> x 
-    <input type="number" v-model="height">
-    Mines:
-    <input type="number" v-model="minesCount">
+    <Field :width="width" :height="height" :minesCount="minesCount" :key="superKey" />
+    <br/>
+    <div class="props">
+      Size: 
+      <input type="number" v-model="width"> x 
+      <input type="number" v-model="height">
+      Mines:
+      <input type="number" v-model="minesCount">
+    </div>
   </div>
 </template>
 
@@ -22,7 +24,12 @@ export default {
     return {
       width: 20,
       height: 10,
-      minesCount: 70,
+      minesCount: 50,
+    }
+  },
+  computed: {
+    superKey: function() {
+      return this.width + '.' + this.height + '.' + this.minesCount;
     }
   }
 }
